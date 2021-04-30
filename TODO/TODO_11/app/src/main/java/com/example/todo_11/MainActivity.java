888ef428fb1844c4fff1cd10ebb2a3e4903ef513
sonamchoki270401 <12190080.gcit@rub.edu.bt>
2021-04-30 22:15:06 +0600

    commit

---
+++ TODO/TODO_11/app/src/main/java/com/example/todo_11/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.todo_11;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+ private TextView helloworld;
+    private String[] mColorArray = {"red", "pink", "purple", "deep_purple",
+            "indigo", "blue", "light_blue", "cyan", "teal", "green",
+            "light_green", "lime", "yellow", "amber", "orange", "deep_orange",
+            "brown", "grey", "blue_grey", "black" };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        helloworld = findViewById(R.id.textview);
+    }
+
+    public void changeColor(View view) {
+        Random random = new Random();
+        String colorName = mColorArray[random.nextInt(20)];
+        int colorResourceName = getResources().getIdentifier(colorName, "color", getApplicationContext().getPackageName());
+        int colorRes = ContextCompat.getColor(this, colorResourceName);
+        helloworld.setTextColor(colorRes);
+    }
+}
