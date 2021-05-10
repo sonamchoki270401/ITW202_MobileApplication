0fdf271a16a397ae212401b9723ba432332e51f9
sonamchoki270401 <12190080.gcit@rub.edu.bt>
2021-05-10 21:07:48 +0600

    commit

---
+++ TODO/TODO_15/app/src/main/java/com/example/todo_15/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.todo_15;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setText("TOP STORIES"));
+        tabLayout.addTab(tabLayout.newTab().setText("TECH NEWS"));
+        tabLayout.addTab(tabLayout.newTab().setText("COOKING"));
+
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        ViewPager viewPager = findViewById(R.id.pager);
+        PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+
+        viewPager.setAdapter(adapter);
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+
+
+        });
+    }
+}
---
+++ TODO/TODO_15/app/src/main/java/com/example/todo_15/PagerAdapter.java
@@ -0,0 +1,34 @@
+package com.example.todo_15;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class PagerAdapter extends FragmentPagerAdapter {
+    int mNumofTabs;
+    public PagerAdapter(@NonNull FragmentManager fm, int behavior){
+        super(fm, behavior);
+        mNumofTabs = behavior;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                return  new Fragment1();
+            case 1:
+                return new Fragment2();
+            case 2:
+                return new Fragment3();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return mNumofTabs;
+    }
+}
