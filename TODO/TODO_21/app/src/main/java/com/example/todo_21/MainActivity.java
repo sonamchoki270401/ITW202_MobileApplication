1440afb330e548bdcffabf125cac5b5f025bb613
sonamchoki270401 <12190080.gcit@rub.edu.bt>
2021-05-26 13:50:40 +0600

    commit

---
+++ TODO/TODO_21/app/src/main/java/com/example/todo_21/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.todo_21;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+private TextView mTextView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mTextView = findViewById(R.id.textview1);
+    }
+
+    public void startTask(View view) {
+        mTextView.setText("napping");
+
+        new SimpleAsyncTask(mTextView).execute();
+
+    }
+}
